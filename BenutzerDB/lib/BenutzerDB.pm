b0784784f2089ba44c7713662d923bd20a7b75b5
Michael Stapelberg <michael@stapelberg.de>
2011-03-09 06:20:41 +0100

    Initial commit

---
+++ BenutzerDB/lib/BenutzerDB.pm
@@ -0,0 +1,100 @@
+# vim:ts=4:sw=4:expandtab
+package BenutzerDB;
+use Dancer ':syntax';
+use Dancer::Plugin::Database;
+use Data::Dumper;
+use Crypt::SaltedHash;
+
+our $VERSION = '0.1';
+
+before sub {
+    # Redirect to login page if necessary (for /my and /admin)
+    if (not session('user') and
+        (request->path_info =~ q,^/my(/?|$), or
+         request->path_info =~ q,^/admin(/?|$),)) {
+        var requested_path => request->path_info;
+        request->path_info('/');
+    }
+};
+
+#
+# displays the login form for invalid sessions and in index page for
+# valid sessions
+#
+get '/' => sub {
+    if (not session('user')) {
+        return template 'login';
+    } else {
+        return template 'index', { user => session('user') };
+    }
+};
+
+post '/login' => sub {
+    return redirect '/' unless exists params->{username} && exists params->{password};
+
+    my $user = params->{username};
+    my $pass = params->{password};
+    my $db = database;
+
+    debug "login attempt with user = $user and pass = $pass";
+
+    my $entry = $db->quick_select('nutzer', { handle => $user });
+    if (!defined($entry) ||
+        !Crypt::SaltedHash->validate($entry->{passwort}, $pass)) {
+        return template 'login', { error => 'Falscher Username/Password' };
+    }
+    
+    session user => $user;
+
+    redirect '/';
+};
+
+any '/logout' => sub {
+    session user => undef;
+    redirect '/';
+};
+
+get '/my/pin' => sub {
+    my $db = database;
+    my $user = session('user');
+
+    my $entry = $db->quick_select('nutzer', { handle => $user });
+    debug 'entry = ' . Dumper($entry);
+    my $pin = $entry->{pin};
+    return template 'mypin', { user => $user, pin => $pin };
+};
+
+get '/register' => sub {
+    template 'register';
+};
+
+post '/register' => sub {
+    if (!exists params->{reg_username} ||
+        !exists params->{reg_password}) {
+        return template 'register', { error => 'Nutzername/Passwort fehlen' };
+    }
+    my $user = params->{reg_username};
+    my $pass = params->{reg_password};
+    my $db = database;
+
+    if (length($pass) < 6) {
+        return template 'register', { error => 'Passwort zu kurz' };
+    }
+
+    my $entry = $db->quick_select('nutzer', { handle => $user });
+    if (defined($entry)) {
+        return template 'register', { error => 'Nutzername schon vergeben!' };
+    }
+
+    my $csh = Crypt::SaltedHash->new(algorithm => 'SHA-1');
+    $csh->add($pass);
+    my $hash = $csh->generate;
+
+    $db->quick_insert('nutzer', { handle => $user, passwort => $hash });
+
+    session user => $user;
+
+    redirect '/';
+};
+
+true;
